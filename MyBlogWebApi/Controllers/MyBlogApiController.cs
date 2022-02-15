35864dca86bc1a965706791da28b160e391c11f1
Marko <marko_poikkimaki@hotmail.com>
2022-02-15 18:38:11 +0100

    Add Routing methods

--- MyBlogWebApi/Controllers/MyBlogApiController.cs
+++ MyBlogWebApi/Controllers/MyBlogApiController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MyBlog.Data.Interfaces;
 using MyBlog.Data.Models;
@@ -21,4 +22,97 @@ public class MyBlogApiController : ControllerBase
     {
         return await _myBlogApi.GetBlogPostsAsync(numberOfPosts, startIndex);
     }
+
+    [HttpGet]
+    [Route("BlogPostCount")]
+    public async Task<int> GetBlogPostAsync()
+    {
+        return await _myBlogApi.GetBlogPostCountAsync();
+    }
+
+    [HttpGet]
+    [Route("BlogPosts/{id}")]
+    public async Task<BlogPost> GetBlogPostAsync(int id)
+    {
+        return await _myBlogApi.GetBlogPostAsync(id);
+    }
+
+    [Authorize]
+    [HttpPut]
+    [Route("BlogPosts")]
+    public async Task<BlogPost> SaveBlogPostAsync([FromBody] BlogPost item)
+    {
+        return await _myBlogApi.SaveBlogPostAsync(item);
+    }
+    
+    [Authorize]
+    [HttpDelete]
+    [Route("BlogPosts")]
+    public async Task DeleteBlogPostAsync([FromBody] BlogPost item)
+    {
+        await _myBlogApi.DeleteBlogPostAsync(item);
+    }
+
+    [HttpGet]
+    [Route("Categories")]
+    public async Task<List<Category>> GetCategoriesAsync()
+    {
+        return await _myBlogApi.GetCategoriesAsync();
+    }
+
+    [HttpGet]
+    [Route("Categories/{id}")]
+    public async Task<Category> GetCategoriesAsync(int id)
+    {
+        return await _myBlogApi.GetCategoryAsync(id);
+    }
+
+    [Authorize]
+    [HttpPut]
+    [Route("Categories")]
+    public async Task<Category> SaveCategoryAsync([FromBody] Category item)
+    {
+        return await _myBlogApi.SaveCategoryAsync(item);
+    }
+
+    [Authorize]
+    [HttpDelete]
+    [Route("Categories")]
+    public async Task DeleteCategoryAsync([FromBody] Category item)
+    {
+        await _myBlogApi.DeleteCategoryAsync(item);
+    }
+
+    [HttpGet]
+    [Route("Tags")]
+    public async Task<List<Tag>> GetTagsAsync()
+    {
+        return await _myBlogApi.GetTagsAsync();
+    }
+
+    [HttpGet]
+    [Route("Tags/{id}")]
+    public async Task<Tag> GetTagsAsync(int id)
+    {
+        return await _myBlogApi.GetTagAsync(id);
+    }
+
+    [Authorize]
+    [HttpPut]
+    [Route("Tags")]
+    public async Task<Tag> SaveTagAsync([FromBody] Tag item)
+    {
+        return await _myBlogApi.SaveTagAsync(item);
+    }
+
+    [Authorize]
+    [HttpDelete]
+    [Route("Tags")]
+    public async Task DeleteTagsAsync([FromBody] Tag tag)
+    {
+        await _myBlogApi.DeleteTagAsync(tag);
+    }
+
+
+
 }
